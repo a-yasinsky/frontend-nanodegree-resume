39c33ba96b4fff0c622e69dca4e8d5ffa5b138ef
a-yasinsky <anatoly@yasinsky.pro>
2017-04-26 21:21:30 +0600

    Fill header with data

--- js/helper.js
+++ js/helper.js
@@ -12,48 +12,51 @@ Cameron Pittman
 These are HTML strings. As part of the course, you'll be using JavaScript functions
 replace the %data% placeholder text you see in them.
 */
-var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheader = {};
+HTMLheader["name"] = '<h1 id="name">%data%</h1>';
+HTMLheader["role"] = '<span>%data%</span><hr>';
+HTMLheader["biopic"] = '<img src="%data%" class="biopic">';
+HTMLheader["welcomeMessage"] = '<span class="welcome-message">%data%</span>';
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
-
-var HTMLbioPic = '<img src="%data%" class="biopic">';
-var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+var HTMLcontacts = {};
+HTMLcontacts["mobile"] = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
+HTMLcontacts["email"] = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
+HTMLcontacts["twitter"] = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
+HTMLcontacts["github"] = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
+HTMLcontacts["location"] = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
 var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
-var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
-var HTMLworkTitle = ' - %data%</a>';
-var HTMLworkDates = '<div class="date-text">%data%</div>';
-var HTMLworkLocation = '<div class="location-text">%data%</div>';
-var HTMLworkDescription = '<p><br>%data%</p>';
+var HTMLwork = [];
+HTMLwork["start"] = '<div class="work-entry"></div>';
+HTMLwork["workEmployer"] = '<a href="#">%data%';
+HTMLwork["workTitle"] = ' - %data%</a>';
+HTMLwork["workDates"] = '<div class="date-text">%data%</div>';
+HTMLwork["workLocation"] = '<div class="location-text">%data%</div>';
+HTMLwork["workDescription"] = '<p><br>%data%</p>';
 
-var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="#">%data%</a>';
-var HTMLprojectDates = '<div class="date-text">%data%</div>';
-var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLproject = [];
+HTMLproject["start"] = '<div class="project-entry"></div>';
+HTMLproject["title"] = '<a href="#">%data%</a>';
+HTMLproject["dates"] = '<div class="date-text">%data%</div>';
+HTMLproject["description"] = '<p><br>%data%</p>';
+HTMLproject["image"] = '<img src="%data%">';
 
-var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
-var HTMLschoolDates = '<div class="date-text">%data%</div>';
-var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+var HTMLschool = [];
+HTMLschool["start"] = '<div class="education-entry"></div>';
+HTMLschool["name"] = '<a href="#">%data%';
+HTMLschool["degree"] = ' -- %data%</a>';
+HTMLschool["dates"] = '<div class="date-text">%data%</div>';
+HTMLschool["location"] = '<div class="location-text">%data%</div>';
+HTMLschool["major"] = '<em><br>Major: %data%</em>';
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
-var HTMLonlineSchool = ' - %data%</a>';
-var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+var HTMLonline = [];
+HTMLonline["classes"] = '<h3>Online Classes</h3>';
+HTMLonline["title"] = '<a href="#">%data%';
+HTMLonline["school"] = ' - %data%</a>';
+HTMLonline["dates"] = '<div class="date-text">%data%</div>';
+HTMLonline["url"] = '<br><a href="#">%data%</a>';
 
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
@@ -63,33 +66,8 @@ var googleMap = '<div id="map"></div>';
 The Internationalize Names challenge found in the lesson Flow Control from JavaScript Basics requires you to create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
 $(document).ready(function() {
-  $('button').click(function() {
-    var $name = $('#name');
-    var iName = inName($name.text()) || function(){};
-    $name.html(iName);
-  });
+  //bio.display();
 });
-
-/*
-The next few lines about clicks are for the Collecting Click Locations quiz in the lesson Flow Control from JavaScript Basics.
-*/
-var clickLocations = [];
-
-function logClicks(x,y) {
-  clickLocations.push(
-    {
-      x: x,
-      y: y
-    }
-  );
-  console.log('x location: ' + x + '; y location: ' + y);
-}
-
-$(document).click(function(loc) {
-  // your code goes here!
-});
-
-
 
 /*
 This is the fun part. Here's where we generate the custom Google Map for the website.
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,10 +1,47 @@
+function fillHTMLobj(fillObj, obj) {
+	var returnObj = {};
+	if(typeof fillObj === 'object' && typeof obj === 'object') {
+		for(var index in fillObj) {
+			if (obj.hasOwnProperty(index)) {
+				returnObj[index] = fillObj[index].replace("%data%",obj[index]);
+			}
+		};
+	}
+	return Object.keys(returnObj).length ? returnObj : fillObj;
+}
+
+function joinObject(obj) {
+	var stringArray = [];
+	if(typeof obj === 'object') {
+		for(var index in obj) {
+			stringArray.push(obj[index]);
+		}
+	}
+	return stringArray.join();
+}
+
+function fillArrayWithTemplate(arr,template) {
+	return Array.isArray(arr) ? arr.map(function(value){
+		return template.replace("%data%",value);
+	}) : arr;
+}
+
 var work = {
 	jobs: [
 	  {
-		employer: "",
-		title: "",
-		dates: "",
-		description: "",
+		employer: "LLC Kyrgyz Concept",
+		title: "The external auditor and consultant",
+		dates: "2013-2015",
+		description: "wwww",
+		location: "Kyrgyzstan",
+		images: [],
+	  },
+	  {
+		employer: "LLC Paida Soft",
+		title: "Founder, CEO",
+		dates: "2013-now",
+		description: "wwww",
+		location: "Kyrgyzstan",
 		images: [],
 	  },
 	],
@@ -22,17 +59,27 @@ var project = {
 };
 
 var bio = {
-	name: "",
-	role: "",
-	welcomeMessage: "",
-	biopic: "",
+	name: "Anatoly Yasinsky",
+	role: "Web Developer",
+	welcomeMessage: "hi there!",
+	biopic: "images/fry.jpg",
 	contacts: {
-		mobile: "",
-		email: "",
-		github: "",
-		location: "",
+		mobile: "+996772002003",
+		email: "anatoly@yasinsky.pro",
+		github: "a-yasinsky",
+		location: "Bishkek, Kyrgyzstan",
 	},
-	skills: [],
+	skills: ["html","css","javascript","jQuery","Knockout.js","php",
+	         "kohana - php framework","yii - php framework", "sql", "1C"],
+	display: function() {
+		var formatedHTMLheader = fillHTMLobj(HTMLheader, this);
+		var formatedContacts = fillHTMLobj(HTMLcontacts, this.contacts);
+		
+		$("#header").prepend(formatedHTMLheader.name,formatedHTMLheader.role);
+		$("#topContacts").append(joinObject(formatedContacts));
+		$("#header").append(formatedHTMLheader.biopic,formatedHTMLheader.welcomeMessage,HTMLskillsStart);
+		$("#skills").append(fillArrayWithTemplate(bio.skills,HTMLskills).join("")); 
+	}
 };
 
 var education = {
@@ -55,3 +102,30 @@ var education = {
 	  },
 	]
 };
+
+bio.display();
+
+/*if (bio.skills.length > 0) {
+	var formattedSkill = "";
+	$("#header").append(HTMLskillsStart);
+	
+	for (var i = 0; i < bio.skills.length; i++) {
+	  formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+	  $("#skills").append(formattedSkill);
+	}
+}
+
+for (job in work.jobs) {
+	$("#workExperience").append(HTMLworkStart);
+	
+	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+	var formatedEmployerTitle = formattedEmployer + formattedTitle;
+	$(".work-entry:last").append(formatedEmployerTitle);
+	var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+	$(".work-entry:last").append(formattedWorkDates);
+	var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+	$(".work-entry:last").append(formattedLocation);
+	var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+	$(".work-entry:last").append(formattedDescription);
+}*/
